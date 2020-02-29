dc0ee6af18832365c74ca3c82d033a3965a4a0c1
Richard Kim <rickiekim@gmail.com>
2020-02-29 17:26:00 -0500

    RData, AIC, Lasso, Ridge
    1. RData
    Switched YrSold to cat; added AgeRemod
    2. AIC - feature selection
    3. Lasso - Lasso/Ridge

--- richard_kim/standardizing test.R
+++ richard_kim/standardizing test.R
@@ -1,24 +1,26 @@
+# Note on Vocab for transforming a column:
+#   Normalize: pull the mean to zero 
+#   Standardize: pull the mean to zero and set sd to one (get the z-score)
+
 # library ####
 library(dplyr)
-library(MASS)
 
-# call data####
-getwd()
+# call data ####
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 load('../Data/train_all.RData')
 
-options(max.print=2000)
+# remove SalePrice and ID####
+# unnecessary for lm
+hp_feat <- hp_feat %>% select(-SalePrice, -Id)
 
-# str(hp_feat)
-# colnames(hp_feat)
-
-hp_feat <- hp_feat %>% dplyr::select(.,-Id, -SalePrice)
+# standardized columns ####
+hp_feat_std <- hp_feat %>% mutate_if(is.numeric, ~(scale(.) %>% as.vector))
 
 # baseline model####
-baselinemodel <- lm(data = hp_feat, formula = SalePrice_log ~ .)
+baselinemodel <- lm(data = hp_feat_std, formula = SalePrice_log ~ .)
 summary(baselinemodel)
 
-
+# setup empty/full models ####
 model.empty = lm(SalePrice_log ~ 1, data = hp_feat) #The model with an intercept ONLY.
 model.full = baselinemodel #The model with ALL variables.
 scope = list(lower = formula(model.empty), upper = formula(model.full))
